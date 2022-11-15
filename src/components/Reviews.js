cc348a5c93cee4f620fcbc47d1c29baadb1b537f
Elaheh <elah0048@stud.kea.dk>
2022-11-15 21:32:00 +0100

    started

---
+++ src/components/Reviews.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react'
+import './Reviews.css'
+import data from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+function Reviews() {
+
+     const [index, setIndex] = useState(0);
+     const {name, job, image, text} = data [index];
+
+     const checkNumber = (number) => {
+         if (number > data.length-1) {
+            return 0
+         }
+         if (number < 0) {
+            return data.length-1
+         }
+         return number
+     }
+
+     const nextSlide = () => {
+          setIndex ((index) => {
+              let newIndex = index+1;
+          //  return newIndex;
+              return checkNumber(newIndex)
+          })
+     };
+
+     const prevSlide = () => {
+          setIndex ((index) => {
+              let newIndex = index-1;
+          //  return newIndex;
+          return checkNumber(newIndex)
+          })
+     }
+
+
+     return (
+          <div className='main'>
+          <h2>Reviews</h2>
+          <div className='reviews'>
+               <div className="review-img">
+                    <img src={image} alt="" />
+               </div>
+               <h4 className="author">
+                    {name}
+               </h4>
+               <p className="job">{job}</p>
+               <p className="info">{text}</p>
+               <div className="review-button">
+                    <button onClick={prevSlide}><FaChevronLeft/></button>
+                    <button onClick={nextSlide}><FaChevronRight /></button>
+               </div>
+               <button className="random-btn">Random</button>
+          </div>
+          </div>
+     )
+}
+
+export default Reviews
---
+++ src/components/data.js
@@ -0,0 +1,33 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'Christian',
+        job: 'Teacher',
+        image: 'https://images.pexels.com/photos/9154716/pexels-photo-9154716.jpeg?cs=srgb&dl=pexels-cottonbro-9154716.jpg&fm=jpg',
+        text: 't is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop'    
+    },
+    {
+        id: 2,
+        name: 'Lars',
+        job: 'Carpenter',
+        image: 'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        text: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also th'    
+    },
+    {
+        id: 3,
+        name: 'Anne',
+        job: 'Programmer',
+        image: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        text: 'making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem'    
+    },
+    {
+        id: 4,
+        name: 'Pol',
+        job: 'Chef',
+        image: 'https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?w=2000',
+        text: 'or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sente'    
+    }
+]
+
+
+export default reviews;
